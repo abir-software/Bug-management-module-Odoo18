2cc91ca38ece996b427e9cda3a5125015faa786f
Md Abir Hassan <pc.dsl@daffodil.family>
2025-07-24 13:47:59 +0600

    update

--- Bug-management-/__manifest__.py
+++ Bug-management-/__manifest__.py
@@ -1,20 +1,40 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Bug Management',
-    'version': '1.0',
-    'summary': 'Module for managing bugs in Odoo',
-    'category': 'Tools',
-    'author': 'Your Company',
-    'website': 'https://yourcompany.com',
-    'depends': ['base'],
+    'name': "Pro QA Bug Management (3D & Animated)",
+    'summary': "Enterprise QA Bug Tracking, 3D Kanban, Bulk Assignment, Dashboard, Analytics",
+    'description': """
+        Next-level QA Bug Management for Odoo 18: animated kanban, stylish dashboards, bulk assignment wizard, analytics.
+        Designed for QA, Developers, and Management teams.
+    """,
+    'version': '18.0.1.0.0',
+    'category': 'Quality',
+    'license': 'LGPL-3',
+    'author': "Md Abir Hassan",
+    'website': "https://www.linkedin.com/in/abirhassan2/",
+    'depends': [
+        'base',
+        'project',
+        'web',
+        'mail',
+        'board',
+    ],
     'data': [
-        'security/management_bug_security.xml',
         'security/ir.model.access.csv',
+        'security/management_bug_security.xml',
+        'data/bugs_data.xml',
         'views/menu.xml',
         'views/management_bug.xml',
         'views/dashboard.xml',
-        'data/bugs_data.xml',
+        'wizard/bug_bulk_assign_view.xml',
     ],
-    'demo': [],
+    'assets': {
+        'web.assets_backend': [
+            'Bug-management-module-Odoo18/static/src/css/bug_management.css',
+        ],
+    },
+    'images': [
+        'static/description/screenshot_management_bug.png',
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
--- Bug-management-/wizard/bug_bulk_assign.py
+++ Bug-management-/wizard/bug_bulk_assign.py
@@ -1,13 +1,17 @@
-# Wizard for bulk assigning bugs placeholder
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class BugBulkAssign(models.TransientModel):
     _name = 'bug.bulk.assign'
     _description = 'Bulk Assign Bugs'
 
-    user_id = fields.Many2one('res.users', string='Assign to User')
-    bug_ids = fields.Many2many('management.bug', string='Bugs')
+    bug_ids = fields.Many2many('bug.management', string="Bugs to Assign")
+    user_id = fields.Many2one('res.users', string="Assign to User", required=True)
 
-    def action_assign(self):
+    def action_assign_bugs(self):
         for bug in self.bug_ids:
-            bug.write({'user_id': self.user_id.id})
+            bug.write({
+                'assigned_to_id': self.user_id.id,
+                'state': 'assigned'
+            })
+            bug.message_post(body=_("Assigned in bulk to %s" % self.user_id.name))
+        return {'type': 'ir.actions.act_window_close'}
